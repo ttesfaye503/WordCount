89096ad87a6c3a4e9b4ee285e02d2fc140404273
Tsion Tesfaye <ttesfaye503@gmail.com>
2021-05-03 16:53:14 -0400

    ImageProcessing

--- ImageProcessing.java
+++ ImageProcessing.java
@@ -1,7 +1,21 @@
 package week4;
 
-import acm.program.GraphicsProgram;
+import acm.program.*; 
+import java.util.*; 
 
-public class ImageProcessing extends GraphicsProgram {
-
+private GImage flipHorizontal(GImage image) {
+int[][] array = image.getPixelArray();
+int width = array[0].length;
+int height = array.length;
+for (int row = 0; 
+row < height; row++) {
+for (int p1 = 0; p1 < width / 2; p1++) 
+{int p2 = width -p1 -1;
+int temp = array[row][p1];array[row][p1] = array[row][p2];
+array[row][p2] = temp;
 }
+}
+return new GImage(array);
+}
+}
+
