65e900324c0104dd9abcb030d0dfe7a72b10b5ab
Tsion Tesfaye <ttesfaye503@gmail.com>
2021-05-03 17:06:34 -0400

    NameCounts

--- NameCounts.java
+++ NameCounts.java
@@ -1,11 +1,30 @@
 package week4;
 
-import acm.program.ConsoleProgram;
-
-public class NameCounts extends ConsoleProgram {
-	
-	public void run() {
-
-	}
-	
+importacm.program.*;importjava.util.*;
+publicclassCountNames extendsConsoleProgram {
+publicvoidrun() {
+HashMap<String,Integer> nameMap = newHashMap<String,Integer>();
+readNames(nameMap);
+printMap(nameMap);
+}
+privatevoidreadNames(Map<String,Integer> map) {
+while(true) {String name = readLine("Enter name: ");
+if(name.equals("")) 
+break;
+Integer count = map.get(name);
+if(count == null) {
+count = newInteger(1);
+} 
+else{count = newInteger(count + 1);
+}
+map.put(name, count);}
+}
+private void printMap(Map<String,Integer> map) {
+Iterator<String> it = map.keySet().iterator();
+while(it.hasNext()) {
+String key = it.next();
+intcount = map.get(key);
+println("Entry [" + key + "] has count " + count);
+}
+}
 }
